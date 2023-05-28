47bab0fa1bb8779ac1da020b2dfcb79372d6adc7
gmatta-2021223 <gmatta-2021223@kinal.edu.gt>
2023-05-28 11:36:11 -0600

    Se copió proyecto de la rama develop a la rama gmatta-2021223

---
+++ RECIBA/src/user/user.routes.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const api = require('express').Router()
+const { ensureAdvance, isMaster, authImg } = require('../services/authenticated');
+const { test, login, register, update, del, updatePassword, save, updateUser, delUser, get, getUser, uploadImg, getImg } = require('./user.controller')
+const connectMultiparty = require('connect-multiparty')
+const upload = connectMultiparty({ uploadDir: './src/uploads/users/' })
+
+api.get('/test', test)
+
+//PUBLIC ROUTES
+api.post('/login', login)
+api.post('/register', register)
+api.get('/getImg/:file', upload, getImg)
+api.put('/registerImg/:id', [upload], uploadImg)
+
+//PRIVATE ROUTES
+api.put('/update', ensureAdvance, update)
+api.delete('/delete', ensureAdvance, del)
+api.put('/updatePassword', ensureAdvance, updatePassword)
+api.put('/uploadImg/:id', [ensureAdvance, authImg, upload], uploadImg)
+
+//ADMIN ROUTES
+api.get('/get', [ensureAdvance, isMaster], get)
+api.get('/get/:id', [ensureAdvance, isMaster], getUser)
+
+//MASTER ROUTES
+api.post('/save', [ensureAdvance, isMaster], save)
+api.put('/update/:id', [ensureAdvance, isMaster], updateUser)
+api.delete('/delete/:id', [ensureAdvance, isMaster], delUser)
+
+module.exports = api
