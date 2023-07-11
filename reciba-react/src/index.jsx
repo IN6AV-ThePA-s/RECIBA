dd6e8141038673d3878b42966587c98502b6afa0
Peek4Boo <jeffer.j4cm@gmail.com>
2023-07-11 00:39:13 -0600

    Vista home principal de usuario

--- reciba-react/src/index.jsx
+++ reciba-react/src/index.jsx
@@ -1,18 +1,31 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import { NotFound } from './pages/NotFound/NotFound'
+import { HomePage } from './pages/HomePage/HomePage'
+import { LoginPage } from './pages/HomePage/LoginPage'
+import { AboutUsPage } from './pages/HomePage/AboutUsPage'
 
 export const Index = () => {
 
     const routes = createBrowserRouter([
         {
             path: '/',
-            element: <App />
-            /* children: [
+            element: <App />,
+            errorElement: <NotFound/>,
+            children: [
                 {
                     path: '/',
-                    element: <HomePage />
+                    element: <HomePage/>
+                },
+                {
+                    path: '/about',
+                    element: <AboutUsPage/>
+                },
+                {
+                    path: '/login',
+                    element: <LoginPage/>
                 }
-            ] */
+            ]
         }
     ])
 
