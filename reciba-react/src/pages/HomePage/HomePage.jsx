458ece651b977adaf5ad6cfd9061aeb9944e0c03
gmatta-2021223 <gmatta-2021223@kinal.edu.gt>
2023-07-11 18:53:13 -0600

    Se copió proyecto de la rama develop a la rama gmatta-2021223

--- reciba-react/src/pages/HomePage/HomePage.jsx
+++ reciba-react/src/pages/HomePage/HomePage.jsx
@@ -15,7 +15,7 @@ export const HomePage = () => {
                 <main className="px-3 mt-auto">
                     <h1 className='h1247'>RECIBA</h1>
                     <p className="p247 lead">
-                        Find a perfect <strong>RECYCLER</strong> to throw away your trash and RECEIVE good rewards.
+                        Find a perfect <strong>RECYCLER</strong> to say goodbye to your trash and hello to RECEIVE good rewards.
                     </p>
                     <p className="lead">
                         <Link to={'/about'} className="btn btn-lg btn-light fw-bold border-white bg-white">More info</Link>
